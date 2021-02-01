e9429dd6f27b95aeacce3deca3cb4b97d70e0125
marks214 <aimee.oz.x@gmail.com>
2021-02-01 11:18:03 -0800

    imported react-router-dom and axios, built out routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Link, Switch, Route, Router } from 'react-router-dom';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [test, setTest] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    axios.get('/')
+      .then(response => {
+        const result = response.data;
+        console.log(result);
+        setTest(result);
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+        console.log(errorMessage);
+      });
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BrowserRouter>
+          <div>
+            {/* navigation bar with links to pages */}
+            <Link className="App-link" to="/">HOME</Link>&nbsp;|&nbsp;
+            <Link className="App-link" to="/search">FOOD SEARCH</Link>&nbsp;|&nbsp;
+            <Link className="App-link" to="/data">DATA</Link>&nbsp;|&nbsp;
+            <Link className="App-link" to="/about">ABOUT</Link>
+          </div>
+          <Switch>
+            <Route exact path='/'>
+              <p>Home placeholder</p>
+            </Route>
+            <Route exact path="/data">
+              <p>user data placeholder</p>
+            </Route>
+            <Route exact path="/about">
+              <p>about page placeholder</p>
+            </Route>
+            <Route path="/:food">
+              <p>food search placeholder</p>
+            </Route>
+          </Switch>
+        </BrowserRouter>
       </header>
     </div>
   );
